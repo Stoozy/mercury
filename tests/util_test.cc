54aea2fbda4496971bcdd8c85af8780c1c0e241e
Stoozy <stoozy@proton.me>
2025-01-24 23:50:29 -0500

    fixing missing field initializer error when building

--- tests/util_test.cc
+++ tests/util_test.cc
@@ -7,13 +7,21 @@ TEST_CASE("DateTime Conversion", "[util]"){
     int MONTH = 1;
     int YEAR = 2014;
 
-    std::tm tm = { /* .tm_sec  = */ 0,
-               /* .tm_min  = */ 0,
-               /* .tm_hour = */ 0,
-               /* .tm_mday = */ (DAY),
-               /* .tm_mon  = */ (MONTH) - 1,
-               /* .tm_year = */ (YEAR) - 1900,
-    };
+    // std::tm tm = { /* .tm_sec  = */ 0,
+    //            /* .tm_min  = */ 0,
+    //            /* .tm_hour = */ 0,
+    //            /* .tm_mday = */ (DAY),
+    //            /* .tm_mon  = */ (MONTH) - 1,
+    //            /* .tm_year = */ (YEAR) - 1900,
+    // };
+
+    std::tm tm;
+    tm.tm_sec = 0;
+    tm.tm_min = 0;
+    tm.tm_hour = 0;
+    tm.tm_mday = (DAY);
+    tm.tm_mon = (MONTH) - 1;
+    tm.tm_year = (YEAR) - 1900;
 
     auto time = util::parse_date("2014-01-09");
     REQUIRE(time == std::chrono::system_clock::from_time_t(std::mktime(&tm)));
